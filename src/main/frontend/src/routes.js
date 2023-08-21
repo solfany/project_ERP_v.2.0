4280aed5c9b69c5db3a6a476d9ddb8dd39142b31
solfany <solfany1999@gmail.com>
2023-08-21 22:41:40 +0900

    #솔비 - 작업 후 중요 파일 종합 커밋
    application, packge, routes 변경

--- src/main/frontend/src/routes.js
+++ src/main/frontend/src/routes.js
@@ -103,7 +103,9 @@ const PayManagementSystemManagement = React.lazy(() =>
 const BulletinBoard = React.lazy(() =>
   import("./views/BulletinBoard/BulletinBoard")
 );
-
+const BulletinBoardPages = React.lazy(() =>
+  import("./views/BulletinBoard/BulletinBoardPages")
+);
 // ========================================================
 // ========================================================
 // ========================================================
@@ -213,6 +215,11 @@ const routes = [
     path: "/BulletinBoard/BulletinBoard",
     name: "BulletinBoard",
     element: BulletinBoard,
+  },
+  {
+    path: "/BulletinBoard/BulletinBoardPages",
+    name: "BulletinBoardPages",
+    element: BulletinBoardPages,
   },
 ];
 
