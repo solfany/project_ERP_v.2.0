d11487f3d76c73491ffda6d6d0ce8685270bc8c4
junyihong <junyihong@naver.com>
2023-08-24 15:33:35 +0900

    #준이 - 백엔드 업데이트
    api 요청 경로 수정

--- src/main/java/com/project/backend/entity/pointshop/Item.java
+++ src/main/java/com/project/backend/entity/pointshop/Item.java
@@ -30,7 +30,6 @@ public class Item extends BaseEntity {
     @Column(nullable = false)
     private int stockNumber;               //재고수량
 
-    @Lob
     @Column(nullable = false)
     private String itemDetail;             //상품 상세설명
 
