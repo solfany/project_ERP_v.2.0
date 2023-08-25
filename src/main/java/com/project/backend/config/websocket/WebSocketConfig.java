f1f22c4c867402b7f33b110ad5da7099312e93b0
cjwo-n <whwjddnjs0@naver.com>
2023-08-25 21:28:05 +0900

    zz

--- src/main/java/com/project/backend/config/websocket/WebSocketConfig.java
+++ src/main/java/com/project/backend/config/websocket/WebSocketConfig.java
@@ -1,28 +1,28 @@
-//package com.project.backend.config.websocket;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.socket.config.annotation.EnableWebSocket;
-//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-//
-//import com.project.backend.websockethandler.WebSocketHandler;
-//
-////@Configuration
-////@EnableWebSocket
-////public class WebSocketConfig implements WebSocketConfigurer {
-////    @Override
-////    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-////        registry.addHandler(new WebSocketHandler(), "/chat").setAllowedOrigins("*");
-////    }
-////}
-//
-//
-//
+package com.project.backend.config.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.project.backend.websockethandler.WebSocketHandler;
+
 //@Configuration
 //@EnableWebSocket
 //public class WebSocketConfig implements WebSocketConfigurer {
 //    @Override
 //    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//        registry.addHandler(new WebSocketHandler(), "/ws").setAllowedOrigins("*");
+//        registry.addHandler(new WebSocketHandler(), "/chat").setAllowedOrigins("*");
 //    }
 //}
+
+
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new WebSocketHandler(), "/ws").setAllowedOrigins("*");
+    }
+}
--- src/main/java/com/project/backend/websockethandler/WebSocketHandler.java
+++ src/main/java/com/project/backend/websockethandler/WebSocketHandler.java
@@ -1,29 +1,29 @@
-//package com.project.backend.websockethandler;
-//
-//import java.util.Map;
-//import java.util.concurrent.ConcurrentHashMap;
-//
-//import org.springframework.web.socket.CloseStatus;
-//import org.springframework.web.socket.TextMessage;
-//import org.springframework.web.socket.WebSocketSession;
-//import org.springframework.web.socket.handler.TextWebSocketHandler;
-//
-//public class WebSocketHandler extends TextWebSocketHandler {
-//    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
-//
-//    @Override
-//    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-//        sessions.put(session.getId(), session);
-//    }
-//
-//    @Override
-//    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-//        // 메시지를 받아 처리하는 로직 작성
-//        // 채팅방 별로 처리 로직을 작성하면 됩니다.
-//    }
-//
-//    @Override
-//    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-//        sessions.remove(session.getId());
-//    }
-//}
+package com.project.backend.websockethandler;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebSocketHandler extends TextWebSocketHandler {
+    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.put(session.getId(), session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // 메시지를 받아 처리하는 로직 작성
+        // 채팅방 별로 처리 로직을 작성하면 됩니다.
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session.getId());
+    }
+}
